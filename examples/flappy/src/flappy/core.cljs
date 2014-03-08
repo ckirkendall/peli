52c9fb1c709cad2c8c370de968619a0651ded340
ckirkendall <ckirkendall@gmail.com>
2014-03-08 10:19:54 -0500

    updated flappy to be infinit

--- examples/flappy/src/flappy/core.cljs
+++ examples/flappy/src/flappy/core.cljs
@@ -9,7 +9,7 @@
             [cljs.core.async :refer (timeout put! chan)])
   (:require-macros [cljs.core.async.macros :refer (go go-loop)]))
 
-(def game-over (TextPrompt. :game-over 100 50 "Game Over" false {}))
+(declare create-objects)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; BUILDING MATERIALS
@@ -39,10 +39,10 @@
                       (schedule-edit 
                        (fn [w]
                          (-> w
-                             (update-in [:bodies] #(conj % game-over))
+                             (update-in [:frame] #(assoc % :x 0 :y 0))
+                             (assoc :bodies (create-objects "Game Over"))
                              (assoc :run-state :paused)))
-                       ch 1)
-                      (assoc b :x 200 :y 200))})
+                       ch 1))})
        this)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -50,6 +50,46 @@
 ;; GAME WORLD
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn create-gate [bodies]
+  (let [start (+ 50 (rand-int 200))
+        x (+ 200 (:x (last bodies)))
+        end (+ start 100)]
+    (conj bodies
+     (Block. (gensym) "#007F00" 50 start x 0 [0 0 5 5])
+     (Block. (gensym) "#007F00" 50 (- 400 end) x end [5 5 0 0]))))
+
+(defn garbage-collect [ch]
+  (go 
+    (while true
+     (<! (timeout 2500))
+     (put! ch
+      {:action :edit-world
+       :fn (fn [w]
+             (if (= (:run-state w) :running)
+               (assoc w :bodies
+                      (vec (filter #(> (+ (:x %) (:width %))
+                                       (:x (:frame w)))
+                                   (:bodies w))))
+                    (:bodies w)))}))))
+
+(defn extend-board [ch]
+  (go
+   (while true
+     (<! (timeout 2500))
+     (put! ch
+           {:action :edit-world
+            :fn (fn [w]
+                  (.log js/console "ADDING A GATE")
+                  (if (= (:run-state w) :running)
+                    (update-in w [:bodies] #(create-gate %))
+                    (:bodies w)))}))))
+
+
+(defn create-objects [text]
+  [(Bird. (gensym) 22 18 100 200 2 0)
+   (Block. (gensym) "#007F00" 50 100 300 0 [0 0 5 5])
+   (Block. (gensym) "#007F00" 50 200 300 200 [5 5 0 0])
+   (TextPrompt. :start 100 50 text false {})])
 
 (def key-actions 
   {32 {:on-down #(assoc-in % [:bodies 0 :vy] 4)}
@@ -61,23 +101,9 @@
    80 {:on-down #(do (println %) %)}})
 
 (def world 
-  (World. {:width 2000 :height 400 :img nil :color "#7F7FFF"}
-          {:width 400 :height 400 :x 0 :y 0}
-          [(Bird. (gensym) 22 18 200 200 2 0)
-           (Block. (gensym) "#007F00" 50  200  300 0 [0 0 5 5])
-           (Block. (gensym) "#007F00" 50 100 300 300 [5 5 0 0])
-           (Block. (gensym) "#007F00" 50 100 500 0 [0 0 5 5])
-           (Block. (gensym) "#007F00" 50 200 500 200 [5 5 0 0])           
-           (Block. (gensym) "#007F00" 50 50 700 0 [0 0 5 5])
-           (Block. (gensym) "#Af8500" 50 250 700 150 [5 5 0 0])
-           
-           (Block. (gensym) "#007F00" 50  200  1200 200 [5 5 0 0])
-           (Block. (gensym) "#007F00" 50 100 1200 0 [0 0 5 5])
-           (Block. (gensym) "#007F00" 50 100 1500 100 [5 5 0 0])
-           (Block. (gensym) "#007F00" 50 200 1500 0 [0 0 5 5])           
-           (Block. (gensym) "#007F00" 50 50 1800 50 [5 5 0 0])
-           (Block. (gensym) "#Af8500" 50 250 1800 0 [0 0 5 5])
-           (TextPrompt. :start 100 50 "Hit Enter" false {})]
+  (World. {:width 20000 :height 400 :img nil :color "#7F7FFF"}
+          {:width 400 :height 400 :x 0 :y 0 :buffer 130}
+          (create-objects "Hit Enter")
           key-actions
           :paused))
 
@@ -90,4 +116,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
   
 (def message-bus (run-game game "myCanvas" :world1))
-
+(extend-board message-bus)
+(garbage-collect message-bus)
