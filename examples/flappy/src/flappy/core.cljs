c0fd82562516501a9e801521234320d1638e336f
ckirkendall <ckirkendall@gmail.com>
2014-03-09 10:41:01 -0400

    code clean up and moving to use edit-loop

--- examples/flappy/src/flappy/core.cljs
+++ examples/flappy/src/flappy/core.cljs
@@ -1,15 +1,41 @@
 (ns flappy.core
   (:require [peli.engine 
              :refer [translate-coords Physics physics Gravity gravity 
-                     Pen draw Collision collide overlap? check-bounds
-                     apply-gravity apply-physics collide-action
-                     collide-solid remove-body play-sound run-game
-                     schedule-edit check-bounds Game World
-                     Block TextPrompt run-game]]
+                     Pen draw Collision collide apply-gravity
+                     apply-physics collide-action collide-solid
+                     remove-body play-sound schedule-edit Game World
+                     Block TextPrompt run-game edit-loop]]
             [cljs.core.async :refer (timeout put! chan)])
   (:require-macros [cljs.core.async.macros :refer (go go-loop)]))
 
 (declare create-objects)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Edit Actions
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn game-over [world]
+  (-> world
+      (update-in [:frame] #(assoc % :x 0 :y 0))
+      (assoc :bodies (create-objects "Game Over"))
+      (assoc :run-state :paused)))
+
+(defn cleanup-gate [world]
+  (if (= (:run-state world) :paused) world
+    (assoc world :bodies
+      (filterv #(> (+ (:x %) (:width %)) (:x (:frame world)))
+               (:bodies world)))))
+
+(defn create-gate [world]
+  (if (= (:run-state world) :paused) world
+    (assoc world :bodies
+      (let [start (+ 50 (rand-int 200))
+            x (+ 200 (:x (last (:bodies world))))
+            end (+ start 100)]
+        (conj (:bodies world)
+          (Block. (gensym) "#007F00" 50 start x 0 [0 0 5 5])
+          (Block. (gensym) "#007F00" 50 (- 400 end) x end [5 5 0 0]))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; BUILDING MATERIALS
@@ -19,7 +45,7 @@
 (defrecord Bird [id width height x y vx vy]
    Pen
   (draw [this ctx frame ch state] 
-    (let [{:keys [width height x y] :as obj} (translate-coords this frame)
+    (let [{:keys [width height x y]} (translate-coords this frame)
           img (.getElementById js/document "bird")] 
       (.drawImage ctx img x y width height)))
   
@@ -35,14 +61,7 @@
    (collide [this body ch state] 
      (condp = (type body)
        Block (collide-action this body 
-              {:any (fn [b]
-                      (schedule-edit 
-                       (fn [w]
-                         (-> w
-                             (update-in [:frame] #(assoc % :x 0 :y 0))
-                             (assoc :bodies (create-objects "Game Over"))
-                             (assoc :run-state :paused)))
-                       ch 10))})
+              {:any (fn [b] (schedule-edit game-over ch 10) b)})
        this)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -50,45 +69,12 @@
 ;; GAME WORLD
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn create-gate [bodies]
-  (let [start (+ 50 (rand-int 200))
-        x (+ 200 (:x (last bodies)))
-        end (+ start 100)]
-    (conj bodies
-     (Block. (gensym) "#007F00" 50 start x 0 [0 0 5 5])
-     (Block. (gensym) "#007F00" 50 (- 400 end) x end [5 5 0 0]))))
-
-(defn garbage-collect [ch]
-  (go 
-    (while true
-     (<! (timeout 2500))
-     (put! ch
-      {:action :edit-world
-       :fn (fn [w]
-             (if (= (:run-state w) :running)
-               (assoc w :bodies
-                      (vec (filter #(> (+ (:x %) (:width %))
-                                       (:x (:frame w)))
-                                   (:bodies w))))
-               w))}))))
-
-(defn extend-board [ch]
-  (go
-   (while true
-     (<! (timeout 2500))
-     (put! ch
-           {:action :edit-world
-            :fn (fn [w]
-                  (if (= (:run-state w) :running)
-                    (update-in w [:bodies] #(create-gate %))
-                    (:bodies w)))}))))
-
 
 (defn create-objects [text]
   [(Bird. (gensym) 22 18 100 200 2 0)
+   (TextPrompt. :start 100 50 text false {})
    (Block. (gensym) "#007F00" 50 100 300 0 [0 0 5 5])
-   (Block. (gensym) "#007F00" 50 200 300 200 [5 5 0 0])
-   (TextPrompt. :start 100 50 text false {})])
+   (Block. (gensym) "#007F00" 50 200 300 200 [5 5 0 0])])
 
 (def key-actions 
   {32 {:on-down #(assoc-in % [:bodies 0 :vy] 4)}
@@ -113,6 +99,6 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
   
-(def message-bus (run-game game "myCanvas" :world1))
-(extend-board message-bus)
-(garbage-collect message-bus)
+(def ch (run-game game "myCanvas" :world1))
+(edit-loop ch :edit-world  cleanup-gate 2500)
+(edit-loop ch :edit-world  create-gate 2500)
