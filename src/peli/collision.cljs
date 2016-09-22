7d52d80a0a1e8e50479c2bf8e4bddd439f983593
Creighton Kirkendall <ckirkendall@gmail.com>
2016-09-22 08:28:27 -0400

    more optimization

--- src/peli/collision.cljs
+++ src/peli/collision.cljs
@@ -1,6 +1,8 @@
 (ns peli.collision
   (:require [peli.geometry :as geo]
-            [clojure.core.matrix :as matrix]))
+            [clojure.core.matrix :as matrix]
+            [peli.vec2 :refer [sub add dot cross-vr cross-rv
+                               cross-vv mul-vr dist-sqr mmul42]]))
 
 
 (defrecord Collision [contacts depth normal a b])
@@ -12,16 +14,11 @@
   (>= a (+ (* b rel-bias)
            (* a abs-bias))))
 
-(defn dist-sqr
-  ([p1 p2] (dist-sqr (matrix/sub p1 p2)))
-  ([[x y]]
-   (+ (* x x) (* y y))))
-
 ;; ---------------------------------------------------------------------
 ;; Circle to Circle Collision
 
 (defn circle->circle [a b]
-  (let [normal (matrix/sub (geo/position b) (geo/position a))
+  (let [normal (sub (geo/position b) (geo/position a))
         dist-sqr (dist-sqr normal)
         total-r (+ (geo/radius a) (geo/radius b))]
     (when (< dist-sqr total-r)
@@ -29,8 +26,8 @@
         (if (= 0.0 dist)
           (Collision. [(geo/position a)] (geo/radius a) [1 0] a b)
           (let [normal (matrix/div normal dist)
-                contacts [(matrix/add (matrix/mmul normal (geo/radius a))
-                                     (geo/position a))]]
+                contacts [(add (mul-vr normal (geo/radius a))
+                               (geo/position a))]]
             (Collision. contacts (- total-r dist) normal a b)))))))
 
 ;; ---------------------------------------------------------------------
@@ -39,14 +36,14 @@
 (defn circle->poly [a b]
   (let [;; Translate a into b's local space
         radius (geo/radius a)
-        center (matrix/mmul (matrix/transpose (geo/rotation-matrix b))
-                           (matrix/sub (geo/position a) (geo/position b)))
+        center (mmul42 (matrix/transpose (geo/rotation-matrix b))
+                       (sub (geo/position a) (geo/position b)))
         points (geo/rel-points b)
         normals (geo/normals b)
         [n-idx sep] (reduce
                      (fn [[n-idx sep] idx]
-                       (let [n-sep (matrix/dot (nth normals idx)
-                                               (matrix/sub center
+                       (let [n-sep (dot (nth normals idx)
+                                               (sub center
                                                  (nth points idx)))]
                          (cond
                            (> n-sep (geo/radius a)) ;; no collision
@@ -65,18 +62,18 @@
     ;; Is center in the polygon
     (when n-idx
       (if (< sep 0.0001)
-        (let [normal (matrix/mul (matrix/mmul (geo/rotation-matrix b)
-                                              (nth normals n-idx)) -1.0)]
-          (Collision. [(matrix/add (matrix/mmul normal radius)
-                                   (geo/position a))]
+        (let [normal (mul-vr (mmul42 (geo/rotation-matrix b)
+                                     (nth normals n-idx)) -1.0)]
+          (Collision. [(add (mul-vr normal radius)
+                            (geo/position a))]
                       radius
                       normal
                       a
                       b))
         ;;find correct voronoi region
         (let [[v1 v2] edge
-              dot1 (matrix/dot (matrix/sub center v1) (matrix/sub v2 v1))
-              dot2 (matrix/dot (matrix/sub center v2) (matrix/sub v1 v2))
+              dot1 (dot (sub center v1) (sub v2 v1))
+              dot2 (dot (sub center v2) (sub v1 v2))
               depth (- radius sep)]
           (cond
             ;;closer to v1
@@ -84,10 +81,10 @@
             (when (<= (dist-sqr center v1)
                       (* radius radius))
               (let [normal (matrix/normalise
-                            (matrix/mmul (geo/rotation-matrix b)
-                                         (matrix/sub v1 center)))
-                    contact (matrix/add (matrix/mmul (geo/rotation-matrix b) v1)
-                                        (geo/position b))]
+                            (mmul42 (geo/rotation-matrix b)
+                                    (sub v1 center)))
+                    contact (add (mmul42 (geo/rotation-matrix b) v1)
+                                 (geo/position b))]
                 (Collision. [contact] depth normal a b)))
 
             ;;closer to v2
@@ -95,25 +92,25 @@
             (when (<= (dist-sqr center v2)
                       (* radius radius))
               (let [normal (matrix/normalise
-                            (matrix/mmul (geo/rotation-matrix b)
-                                         (matrix/sub v2 center)))
-                    contact (matrix/add (matrix/mmul (geo/rotation-matrix b) v2)
-                                        (geo/position b))]
+                            (mmul42 (geo/rotation-matrix b)
+                                    (sub v2 center)))
+                    contact (add (mmul42 (geo/rotation-matrix b) v2)
+                                 (geo/position b))]
                 (Collision. [contact] depth normal a b)))
 
             ;;closest to face
             :else
-            (let [normal (matrix/mul (matrix/mmul (geo/rotation-matrix b)
-                                                   (nth normals n-idx))
-                                      -1.0)
-                  contact (matrix/add (matrix/mul normal radius)
-                                      (geo/position a))]
+            (let [normal (mul-vr (mmul42 (geo/rotation-matrix b)
+                                         (nth normals n-idx))
+                                 -1.0)
+                  contact (add (mul-vr normal radius)
+                               (geo/position a))]
               (Collision. [contact] depth normal a b))))))))
 
 
 (defn poly->circle [body1 body2]
   (when-let [collision (circle->poly body2 body1)]
-    (-> (update collision :normal matrix/mmul -1.0)
+    (-> (update collision :normal mul-vr -1.0)
         (assoc :a (:b collision))
         (assoc :b (:a collision)))))
 
@@ -124,11 +121,11 @@
   (let [[p1 & points] (geo/rel-points body)]
     (first
      (reduce (fn [[best-point best-proj] point]
-               (let [proj (matrix/dot point dir)]
+               (let [proj (dot point dir)]
                  (if (> proj best-proj)
                    [point proj]
                    [best-point best-proj])))
-             [p1 (matrix/dot p1 dir)]
+             [p1 (dot p1 dir)]
              points))))
 
 
@@ -143,16 +140,16 @@
     (reduce (fn [[best-axis depth] idx]
               (let [;; retrieve face normal from A
                     n-tmp (nth normals idx)
-                    nw (matrix/mmul a-rot-mat n-tmp)
+                    nw (mmul42 a-rot-mat n-tmp)
                     ;;transform faces normal to B's local space
-                    norm (matrix/mmul b-rot-mat-trans nw)
+                    norm (mmul42 b-rot-mat-trans nw)
                     ;;retrieve support point
-                    sup-point (get-support b (matrix/mul norm -1.0))
+                    sup-point (get-support b (mul-vr norm -1.0))
                     ;;retrieve vertex on A trans into B's space
-                    v-tmp (matrix/add (matrix/mmul a-rot-mat (nth points idx))
+                    v-tmp (add (mmul42 a-rot-mat (nth points idx))
                                       a-pos)
-                    v-tmp (matrix/mmul b-rot-mat-trans (matrix/sub v-tmp b-pos))
-                    n-depth (matrix/dot norm (matrix/sub sup-point v-tmp))]
+                    v-tmp (mmul42 b-rot-mat-trans (sub v-tmp b-pos))
+                    n-depth (dot norm (sub sup-point v-tmp))]
                 (if (> n-depth depth)
                   [idx n-depth]
                   [best-axis depth])))
@@ -169,20 +166,20 @@
         b-pos (geo/position b)
         ref-norm-tmp (nth a-norms ref-idx)
         ;;translate to frame of reference
-        ref-norm (matrix/mmul
-                  (matrix/transpose (geo/rotation-matrix b))
-                  (matrix/mmul a-rot-mat ref-norm-tmp))
+        ref-norm (mmul42
+                  (matrix/transpose b-rot-mat)
+                  (mmul42 a-rot-mat ref-norm-tmp))
         [iface min-dot] (reduce (fn [[iface min-dot] idx]
-                                  (let [dot (matrix/dot ref-norm
+                                  (let [dot (dot ref-norm
                                                         (nth b-norms idx))]
                                     (if (< dot min-dot)
                                       [idx dot]
                                       [iface min-dot])))
                                 [0 js/Number.POSITIVE_INFINITY]
                                 (range (count b-points)))
-        v1 (matrix/add (matrix/mmul b-rot-mat (nth b-points iface)) b-pos)
+        v1 (add (mmul42 b-rot-mat (nth b-points iface)) b-pos)
         iface2 (mod (inc iface) (count b-points))
-        v2 (matrix/add (matrix/mmul b-rot-mat (nth b-points iface2)) b-pos)]
+        v2 (add (mmul42 b-rot-mat (nth b-points iface2)) b-pos)]
     [v1 v2]))
 
 
@@ -190,15 +187,14 @@
   (let [[v1 v2] face
         ;;distance from points to line
         ;; d = ax + by -c
-        d1 (- (matrix/dot norm v1) c)
-        d2 (- (matrix/dot norm v2) c)
+        d1 (- (dot norm v1) c)
+        d2 (- (dot norm v2) c)
         [o1 o2] [(if (and (> d1 0.0)
                           (<= d2 0.0))
                    v2 v1) v2]
         o2 (if (< (* d1 d2) 0.0)
              (let [alpha (/ d1 (- d1 d2))]
-               (matrix/add v1 (matrix/mul (matrix/sub v2 v1)
-                                          alpha)))
+               (add v1 (mul-vr (sub v2 v1) alpha)))
              o2)]
     [(cond-> 0
        (<= d1 0.0) inc
@@ -210,10 +206,8 @@
 
 (defn poly->poly [orig-a orig-b]
   (let [[a-idx depth-a] (find-axis-least-depth orig-a orig-b)]
-    #_(println "A:" orig-a a-idx depth-a)
     (when-not (>= depth-a 0.0)
       (let [[b-idx depth-b] (find-axis-least-depth orig-b orig-a)]
-        #_(println "B:" orig-b b-idx depth-b)
         (when-not (>= depth-b 0.0)
           (let [[a b idx flip depth] (if (bias-greater-than depth-a depth-b)
                                        [orig-a orig-b a-idx false depth-a]
@@ -225,17 +219,17 @@
                 v1-tmp (nth a-points idx)
                 v2-tmp (nth a-points (mod (inc idx) (count a-points)))
                 ;; trans verts into world space
-                v1 (matrix/add (matrix/mmul a-rot-mat v1-tmp) a-pos)
-                v2 (matrix/add (matrix/mmul a-rot-mat v2-tmp) a-pos)
+                v1 (add (mmul42 a-rot-mat v1-tmp) a-pos)
+                v2 (add (mmul42 a-rot-mat v2-tmp) a-pos)
                 ;; side plane normal in world space
-                [sx sy :as side-plane-norm] (matrix/normalise (matrix/sub v2 v1))
+                [sx sy :as side-plane-norm] (matrix/normalise (sub v2 v1))
                 ;; orthogonalize
                 ref-face-norm [sy (* sx -1.0)]
 
-                ref-c (matrix/dot ref-face-norm v1)
-                neg-side (* -1.0 (matrix/dot side-plane-norm v1))
-                pos-side (matrix/dot side-plane-norm v2)
-                [sp incident-face] (clip (matrix/mmul side-plane-norm -1.0)
+                ref-c (dot ref-face-norm v1)
+                neg-side (* -1.0 (dot side-plane-norm v1))
+                pos-side (dot side-plane-norm v2)
+                [sp incident-face] (clip (mul-vr side-plane-norm -1.0)
                                           neg-side
                                           incident-face)]
             (when (= sp 2) ;;floating point error if not 2
@@ -244,12 +238,12 @@
                                               incident-face)]
                 (when (= sp 2) ;;floating point error if not 2
                   (let [[ifn1 ifn2] incident-face
-                        d-tmp (- (matrix/dot ref-face-norm ifn1) ref-c)
+                        d-tmp (- (dot ref-face-norm ifn1) ref-c)
                         col-tmp (if (<= d-tmp 0.0)
                                   (map->Collision {:contacts [ifn1]
                                                    :depth (* -1.0 d-tmp)})
                                   (map->Collision {:depth 0}))
-                        d-tmp (- (matrix/dot ref-face-norm ifn2) ref-c)
+                        d-tmp (- (dot ref-face-norm ifn2) ref-c)
                         col-tmp (if (<= d-tmp 0.0)
                                   (-> col-tmp
                                       (update :contacts (fnil conj []) ifn2)
@@ -259,7 +253,7 @@
                         (assoc :a orig-a :b orig-b)
                         (update :depth / (double (count (:contacts col-tmp))))
                         (assoc :normal (if flip
-                                         (matrix/mul ref-face-norm -1.0)
+                                         (mul-vr ref-face-norm -1.0)
                                          ref-face-norm)))))))))))))
 
 
